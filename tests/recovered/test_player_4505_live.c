28b58dd0b44d1a30d16c216ec166aa17d2e285b0
Celso Fontes <celsowm@users.noreply.github.com>
2026-09-25 17:48:13 -0300

    recover player state mask 802059f

--- tests/recovered/test_player_4505_live.c
+++ tests/recovered/test_player_4505_live.c
@@ -685,6 +685,9 @@ int main(int argc, char **argv)
     } else if (argc == 3 && strcmp(argv[2], "--mask-402059f") == 0) {
         test_quadruple_only = 1;
         test_quadruple_mask = UINT32_C(0x0402059f);
+    } else if (argc == 3 && strcmp(argv[2], "--mask-802059f") == 0) {
+        test_quadruple_only = 1;
+        test_quadruple_mask = UINT32_C(0x0802059f);
     } else if (argc == 3 && strcmp(argv[2], "--mask-0400059f") == 0) {
         test_quadruple_only = 1;
         test_quadruple_mask = UINT32_C(0x0400059f);
