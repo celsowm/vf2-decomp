d4aad8bafec9925c3d1f6f2934aefc6d51726fe5
Celso Fontes <celsowm@users.noreply.github.com>
2026-09-25 17:57:44 -0300

    recover player state mask 48059f

--- src/recovered/hybrid.c
+++ src/recovered/hybrid.c
@@ -1158,7 +1158,7 @@ static vf2_status hybrid_execute_player_19ef8(
      * measured non-branch masks 0x0f, 0x17, 0x1b, 0x1d, 0x1e, 0x1f and
      * 0x9f, 0x19f, 0x59f, 0x1059f, 0x5059f, 0x8059f, 0x18059f,
      * 0x0002059f, 0x0006059f, 0x000a059f, 0x0012059f, 0x0042059f,
-     * 0x0004059f, 0x000c059f, 0x0014059f, 0x0102059f, 0x0402059f, 0x0802059f, 0x1002059f,
+     * 0x0004059f, 0x000c059f, 0x0014059f, 0x0048059f, 0x0102059f, 0x0402059f, 0x0802059f, 0x1002059f,
      * 0x4002059f, 0x8002059f and
      * 0x0040059f, 0x0100059f, 0x0200059f, 0x0400059f, 0x0800059f,
      * 0x1000059f, 0x4000059f and 0x8000059f with their branch-bit
@@ -1197,6 +1197,7 @@ static vf2_status hybrid_execute_player_19ef8(
             non_branch_state_flags == UINT32_C(0x0012059f) ||
             non_branch_state_flags == UINT32_C(0x000c059f) ||
             non_branch_state_flags == UINT32_C(0x0014059f) ||
+            non_branch_state_flags == UINT32_C(0x0048059f) ||
             non_branch_state_flags == UINT32_C(0x0042059f) ||
             non_branch_state_flags == UINT32_C(0x0102059f) ||
             non_branch_state_flags == UINT32_C(0x0402059f) ||
